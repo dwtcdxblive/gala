9903a8ba50a1c5526f5f7ecac1a420d08c3dbeb7
dwtcdxblive <omar.aouf@dwtc.com>
2024-06-03 13:17:44 +0400

    hostess page updated

--- src/components/HostessPage.js
+++ src/components/HostessPage.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import VIP from '../assets/vip.png';
-import Logo from '../assets/logo.png';
 import Banner from '../assets/detbanner.png';
+
 function HostessPage() {
   const [guestList, setGuestList] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
-  const [isCheckedIn, setIsCheckedIn] = useState(false);
 
   const fetchGuestData = () => {
     // Fetch the guest list from your backend
-    axios.get('http://46.19.74.196:5000/api/guests/absent').then((response) => {
+    axios.get('http://46.19.74.196:5000/api/guests').then((response) => {
       setGuestList(response.data);
     });
   };
@@ -36,7 +34,9 @@ function HostessPage() {
       .then((response) => {
         console.log('Guest marked as attended:', response.data);
         setGuestList((prevGuestList) =>
-          prevGuestList.filter((guest) => guest._id !== guestId)
+          prevGuestList.map((guest) =>
+            guest._id === guestId ? { ...guest, checkedIn: true } : guest
+          )
         );
       })
       .catch((error) => {
@@ -51,33 +51,17 @@ function HostessPage() {
 
   // Filter the guestList based on the searchQuery
   const filteredGuestList = guestList.filter((guest) => {
-         const fullName = (guest.fullname || '').toLowerCase();
-         const query = searchQuery.toLowerCase();
+    const fullName = (guest.fullname || '').toLowerCase();
+    const query = searchQuery.toLowerCase();
 
-         return (
-           fullName.startsWith(query) ||
-           (guest.firstname &&
-             guest.firstname.toLowerCase().startsWith(query)) ||
-           (guest.lastname && guest.lastname.toLowerCase().startsWith(query)) 
-            || (guest.galaGuestFullName && guest.galaGuestFullName.toLowerCase().startsWith(query))
-         );
+    return (
+      fullName.startsWith(query) ||
+      (guest.firstname && guest.firstname.toLowerCase().startsWith(query)) ||
+      (guest.lastname && guest.lastname.toLowerCase().startsWith(query)) ||
+      (guest.galaGuestFullName &&
+        guest.galaGuestFullName.toLowerCase().startsWith(query))
+    );
   });
-
-  const handleCheckIn = (guestId) => {
-    // Perform the check-in logic here, e.g., make an API call to update the check-in status
-    axios
-      .put(`http://46.19.74.196:5000/api/guests/${guestId}`, {
-        action: 'attended',
-      })
-      .then((response) => {
-        setIsCheckedIn(true);
-      })
-      .catch((error) => {
-        console.error('Error checking in guest:', error);
-        // Handle the error if needed
-      });
-  };
-
 
   return (
     <div>
@@ -85,10 +69,9 @@ function HostessPage() {
         <div className='container-fluid'>
           <div className='row justify-content-center'>
             <div className='col-md-12 d-flex justify-content-center p-0'>
-              <img src={Banner} />
+              <img src={Banner} alt='Banner' />
             </div>
-            <div className='col-md-12 my-3'>
-            </div>
+            <div className='col-md-12 my-3'></div>
             <div className='col-md-12'>
               <div className='input-group mb-3'>
                 <div className='input-group mb-3'>
@@ -116,7 +99,7 @@ function HostessPage() {
                       <tr>
                         <th>Name</th>
                         <th>Title</th>
-                        <th>organization</th>
+                        <th>Organization</th>
                         <th>FF/GC</th>
                         <th>Actions</th>
                       </tr>
@@ -131,13 +114,23 @@ function HostessPage() {
                           <td>{guest.organization}</td>
                           <td>{guest.localinternational}</td>
                           <td>
-                            <button
-                              type='button'
-                              className='btn btn-outline-success'
-                              onClick={() => markAsAttended(guest._id)}
-                            >
-                              Check in
-                            </button>
+                            {guest.checkedIn ? (
+                              <button
+                                type='button'
+                                className='btn btn-outline-secondary'
+                                disabled
+                              >
+                                Checked In
+                              </button>
+                            ) : (
+                              <button
+                                type='button'
+                                className='btn btn-outline-success'
+                                onClick={() => markAsAttended(guest._id)}
+                              >
+                                Check in
+                              </button>
+                            )}
                           </td>
                         </tr>
                       ))}
