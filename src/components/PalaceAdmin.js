2e2fe9cdadc3f59437cc9fd9cca61319ac036b01
dwtcdxblive <omar.aouf@dwtc.com>
2024-06-04 16:34:27 +0400

    cookie preference

--- src/components/PalaceAdmin.js
+++ src/components/PalaceAdmin.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
+import Cookies from 'js-cookie';
 import HostessPage from './HostessPage';
 import PalaceCheckIn from './PalaceCheckIn';
 
 function PalacePointPage() {
+  const [defaultTab, setDefaultTab] = useState('hostess');
+
+  useEffect(() => {
+    const savedTab = Cookies.get('defaultTab');
+    if (savedTab) {
+      setDefaultTab(savedTab);
+    }
+  }, []);
+
+  const handleTabSelect = (tab) => {
+    setDefaultTab(tab);
+    Cookies.set('defaultTab', tab, { expires: 30 }); // Save the tab selection in cookies for 30 days
+  };
+
   return (
     <div>
-      <Tabs defaultActiveKey='hostess' id='palacepoint-tabs' className='mb-3'>
+      <Tabs
+        activeKey={defaultTab}
+        onSelect={handleTabSelect}
+        id='palacepoint-tabs'
+        className='mb-3'
+      >
         <Tab eventKey='hostess' title='Check-in'>
           <HostessPage />
         </Tab>
-        <Tab eventKey='palacecheckin' title='Badge Preperation'>
+        <Tab eventKey='palacecheckin' title='Badge Preparation'>
           <PalaceCheckIn />
+        </Tab>
+        <Tab eventKey='thirdtab' title='Third Tab'>
+          {/* Add your third tab content here */}
+          <div>Third Tab Content</div>
         </Tab>
       </Tabs>
     </div>
